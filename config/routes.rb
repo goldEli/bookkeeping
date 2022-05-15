4754c3f3166a135a63507680ae48738175aae17b
miaoyu <miaoyu2009@qq.com>
2022-05-15 09:34:34 +0800

    feat: create validationCodeController

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
-      resource: :session, only: [:create, :destroy]
-      resource: :me, only: [:show]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
       resources :items
-      resources: :tags
+      resources :tags
     end
   end
 end
